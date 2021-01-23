198e9d75f4bcac1916be98e1e39e0644db3b56b2
문규찬 <lazychan90@gmail.com>
2021-01-24 05:45:08 +0900

    초기 세팅2

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,16 +1,2 @@
-import React from "react";
-import "react-router-dom";
-import "./Main.scss";
 
-class Main extends React.Component{
-  render(){
-    return(
-      <div classNam="Main">
-        <div>안녕하세요?</div>
-      </div>
-    )
-  }
-}
-
-export default Main;
 
