ac066f4e97ee58a8439fffafbe2e9b5cf14b3b1f
문규찬 <lazychan90@gmail.com>
2021-01-24 05:38:59 +0900

    초기 세팅

---
+++ src/Pages/Main/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "react-router-dom";
+import "./Main.scss";
+
+class Main extends React.Component{
+  render(){
+    return(
+      <div classNam="Main">
+        <div>안녕하세요?</div>
+      </div>
+    )
+  }
+}
+
+export default Main;
+
---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { Main } from "./Pages/Main/Main";
+import { Details} from "./Pages/Details/Details";
+
+class Routes extends React.Component{
+  render(){
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" Component={Main} />
+          <Route exact path="/Details" Component={Details} />
+        </Switch>
+      </Router>
+    )
+  }
+}
+
+export default Routes;
