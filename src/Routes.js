ea3ece22a016f5f14fc1ae999bcdb7a0f4906a6c
문규찬 <lazychan90@gmail.com>
2021-03-10 15:47:05 +0900

    useState/useEffect 연습중

--- src/Routes.js
+++ src/Routes.js
@@ -2,18 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Main } from "./Pages/Main/Main";
-import { Details} from "./Pages/Details/Details";
+import { Details } from "./Pages/Details/Details";
+import { Counter } from "./velopert/Counter";
+import { Info } from "./velopert/Info";
 
-class Routes extends React.Component{
-  render(){
+class Routes extends React.Component {
+  render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" Component={Main} />
-          <Route exact path="/Details" Component={Details} />
+          <Route exact path="/main" component={Main} />
+          <Route exact path="/details" component={Details} />
+          <Route exact path="/" component={Counter} />
+          <Route exact path="/info" component={Info} />
         </Switch>
       </Router>
-    )
+    );
   }
 }
 
